caa5f322a95161932be9f664d4a74680958c6136
wangjianpeng <wangjianpeng@weimiao.cn>
2022-03-15 11:08:42 +0800

    1

---
+++ 20.有效的括号.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=javascript
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (char === "(" || char === "{" || char === "[") {
+      stack.push(char);
+    } else {
+      if (stack.length === 0) return false;
+
+      const left = stack.pop();
+      if (left === "(" && char !== ")") return false;
+      if (left === "{" && char !== "}") return false;
+      if (left === "[" && char !== "]") return false;
+    }
+  }
+
+  return stack.length === 0;
+};
+// @lc code=end
